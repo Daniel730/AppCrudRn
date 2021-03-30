cfba61f3e72f7011c8cd88d9b61c2aefcc74cc23
Daniel Silva <daniel.silva@direitodeouvir.com.br>
2021-03-30 10:36:31 -0300

    Preparação do projeto

---
+++ src/data/users.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        id: 1,
+        name: "Dan Silva",
+        email: "daniel.silva@direitodeouvir.com.br",
+        avatarUrl: "https://cdn.pixabay.com/photo/2016/05/30/14/23/detective-1424831_960_720.png"
+    },
+    {
+        id: 2,
+        name: "André Garcia",
+        email: "andre.garcia@direitodeouvir.com.br",
+        avatarUrl: "https://cdn.pixabay.com/photo/2016/03/31/21/09/disguise-1296221_960_720.png"
+    },
+    {
+        id: 3,
+        name: "Thiago Galdiano",
+        email: "ti@direitodeouvir.com.br",
+        avatarUrl: "https://cdn.pixabay.com/photo/2016/04/25/07/15/man-1351317_960_720.png"
+    },
+    {
+        id: 4,
+        name: "Angélica Souza",
+        email: "ange.souza.silva@gmail.com",
+        avatarUrl: "https://cdn.pixabay.com/photo/2021/03/12/08/01/child-6089047_960_720.png"
+    }
+]
